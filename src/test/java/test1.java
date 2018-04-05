523aee8a6679270d85c601205ef840177cd0da5a
rocamadour <flea2890@gmail.com>
2018-04-05 22:11:19 +0200

    uno

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -1,15 +1,19 @@
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Created by rocamadour on 05/04/18.
  */
-public class FirstTestCase {
+public class test1 {
 
+    @Test
     public static void main(String[] args) {
 
+        System.setProperty("webdriver.chrome.driver", "/home/rocamadour/Automation/Chromedriver/chromedriver");
         // Create a new instance of the Firefox driver
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new ChromeDriver();
 
         //Launch the Online Store Website
         driver.get("http://www.store.demoqa.com");
@@ -17,10 +21,6 @@ public class FirstTestCase {
         // Print a Log In message to the screen
         System.out.println("Successfully opened the website www.Store.Demoqa.com");
 
-        //Wait for 5 Sec
-        Thread.sleep(5000);
-
         // Close the driver
         driver.quit();
-    }
 }
